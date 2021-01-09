e4fb501240ca355d42a5f59d6a8826dc2534625f
RaymonvdK <raymon-v-d-k@hotmail.com>
2021-01-09 12:00:16 +0100

    Extended my prior bugfix

--- app/Http/Controllers/DealController.php
+++ app/Http/Controllers/DealController.php
@@ -52,19 +52,18 @@ class DealController extends Controller
      */
     protected function getDeals(): Collection
     {
-        $request = null;
         try {
             //We put this code in a try/catch because the enpoint could fail. 
-            $request = (new Client)->get('https://media.socialdeal.nl/demo/deals.jso');
+            $dealRequest = (new Client)->get('https://media.socialdeal.nl/demo/deals.json');
+            
+            //If the endpoint returned data we will return it as a collection so it can be used like Eloquent. It's sorted if present in the HTTP request.
+            return collect(json_decode($dealRequest->getBody())->deals)->sortBy(request()->sorting);
         } catch (Exception $e) {
-            //Here we log what happend. Could implement BugSnag here. 
+            //Here we log what happend. Could also implement BugSnag. 
             Log::error('Retrieving the deals failed.');
 
-            //We return an empty collection because the endpoint failed. In the view it will handle the empty collection. 
+            //We return an empty collection because the endpoint failed or the body does not have any deals. In the view it will handle the empty collection. 
             return new Collection();
         }
-        
-        //If the endpoint returned data we will return it as a collection so it can be used like Eloquent. It's sorted if present in the request.
-        return collect(json_decode($request->getBody())->deals)->sortBy(request()->sorting);
     }
 }
